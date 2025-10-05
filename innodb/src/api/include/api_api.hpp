d64703dd03ac53d89873f83ae4bfb163b22f4a14
Gitlab CI <fabio.filasieno@gmail.com>
2025-10-05 23:34:55 +0200

    review api headers

--- innodb/src/api/include/api_api.hpp
+++ innodb/src/api/include/api_api.hpp
@@ -1,73 +1,56 @@
-/***********************************************************************
-Copyright (c) 2008, 2025 Innobase Oy. All Rights Reserved.
-Copyright (c) 2008, 2009 Oracle. All Rights Reserved.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; version 2 of the License.
+// Copyright (c) 2025 Fabio N. Filasieno. All Rights Reserved.
+// Copyright (c) 2008, 2025 Innobase Oy. All Rights Reserved.
+// Copyright (c) 2008, 2009 Oracle. All Rights Reserved.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+/// \file  api_api.hpp
+/// \brief InnoDB API header file
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#pragma once
 
-************************************************************************/
-
-/// @file api_api.hpp
-/// \brief HailDB API header file
-
-#ifndef INNOBASE_API_H
-#define INNOBASE_API_H
-
+// TODO: Review inclues
 #include "db_err.hpp"
-
-#include <stdarg.h>
 #include "haildb.hpp"
+#include <stdarg.h>
 
-/*********************************************************************/ /**
-Declare private functions that should not be visible in the public API
-below, outside of API_BEGIN_INCLUDE/API_END_INCLUDE.
-*************************************************************************/
-
-/*********************************************************************/ /**
-Execute arbitrary SQL using InnoDB's internal parser. The statement
-is executed in a new transaction. Table name parameters must be prefixed
-with a '$' symbol and variables with ':'
-@return	DB_SUCCESS or error code 
-\param sql to execute
-\param n_args no. of args
-
-*/
-
+/// \brief Execute arbitrary SQL using InnoDB's internal parser. 
+/// \details The statement is executed in a new transaction. Table name parameters must be prefixed
+/// with a '$' symbol and variables with ':'
+/// \param sql to execute
+/// \param n_args no. of args
+/// \return	DB_SUCCESS or error code 
+/// \internal
 ib_err_t ib_exec_sql(const char *sql, ib_ulint_t n_args, ...);
 
-/*********************************************************************/ /**
-Execute arbitrary SQL using InnoDB's internal parser. The statement
-is executed in a background transaction. It will lock the data
-dictionary lock for the duration of the query.
-\return	DB_SUCCESS or error code 
-\param sql sql to execute
-\param n_args no of args
-*/
+/// \brief Execute arbitrary SQL using InnoDB's internal parser. 
+// The statement is executed in a background transaction. It will lock the data
+/// dictionary lock for the duration of the query.
+/// \return	DB_SUCCESS or error code 
+/// \param sql sql to execute
+/// \param n_args no of args
 ib_err_t ib_exec_ddl_sql(const char *sql, ib_ulint_t n_args, ...);
 
-/// Initialize the config system.
-/// @return	DB_SUCCESS or error code
+/// \brief Initialize the config system.
+/// \return	DB_SUCCESS or error code
 ib_err_t ib_cfg_init(void);
 
-/// Shutdown the config system.
-/// @return	DB_SUCCESS or error code
+/// \brief Shutdown the config system.
+/// \return	DB_SUCCESS or error code
 ib_err_t ib_cfg_shutdown(void);
 
 extern int srv_panic_status;
-#define IB_CHECK_PANIC()             \
-    do {                             \
-        if (srv_panic_status)        \
-            return srv_panic_status; \
-    } while (0)
 
-#endif /* INNOBASE_API_H */
+#define IB_CHECK_PANIC() do { if (srv_panic_status) return srv_panic_status; } while (0)
--- innodb/src/api/include/api_misc.hpp
+++ innodb/src/api/include/api_misc.hpp
@@ -15,7 +15,6 @@ Copyright (c) 2008 Oracle. All Rights Reserved.
 #include "que_que.hpp"
 #include "trx_trx.hpp"
 
-
 /// \brief Determines if the currently running transaction has been interrupted.
 /// \param trx Transaction.
 /// \return TRUE if interrupted.
@@ -28,22 +27,16 @@ UNIV_INTERN int ib_create_tempfile(const char* filename);
 	
 /// \brief Handles user errors and lock waits detected by the database engine.
 /// \return TRUE if it was a lock wait and we should continue running the query thread.
-UNIV_INTERN
-ibool
-ib_handle_errors(enum db_err* new_err, trx_t* trx, que_thr_t* thr, trx_savept_t* savept);
+UNIV_INTERN ibool ib_handle_errors(enum db_err* new_err, trx_t* trx, que_thr_t* thr, trx_savept_t* savept);
 
 /// \brief Sets a lock on a table.
 /// \param trx Transaction.
 /// \param table Table to lock.
 /// \param mode Lock mode.
 /// \return Error code or DB_SUCCESS.
-UNIV_INTERN
-enum db_err
-ib_trx_lock_table_with_retry(trx_t* trx, dict_table_t* table, enum lock_mode mode);
+UNIV_INTERN enum db_err ib_trx_lock_table_with_retry(trx_t* trx, dict_table_t* table, enum lock_mode mode);
 
 /// \brief Updates the table modification counter and calculates new estimates
 /// for table and index statistics if necessary.
 /// \param table Table.
-UNIV_INTERN
-void
-ib_update_statistics_if_needed(dict_table_t* table);
+UNIV_INTERN void ib_update_statistics_if_needed(dict_table_t* table);
