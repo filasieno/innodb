fadb222aa93c7a3ca6370e1392b5aa366ab5ba1d
Gitlab CI <fabio.filasieno@gmail.com>
2025-11-04 17:59:31 +0100

    fix tail_call.cpp

--- xinnodb/src/task/example/tail_call.cpp
+++ xinnodb/src/task/example/tail_call.cpp
@@ -13,7 +13,7 @@ IB_ASYNC do_print4(int x, int y, int z) {
         std::exit(0);
     } else {
         fmt::print("4 <=\n");
-        ib_tail_call(do_print4, 1, 2, 3);
+        co_do(do_print4, 1, 2, 3);
     }
 }
 
@@ -24,10 +24,10 @@ IB_ASYNC do_print3(void* ptr = nullptr) {
     int val = int_dist(gen);
     if (val > 50) {
         fmt::print("3 >\n");
-        ib_tail_call(do_print4, 1, 2, 3);
+        co_do(do_print4, 1, 2, 3);
     } else {
         fmt::print("3 <=\n");
-        ib_tail_call(do_print3, ptr);
+        co_do(do_print3, ptr);
     }
 }
 
@@ -38,10 +38,10 @@ IB_ASYNC do_print2(void* ptr = nullptr) {
     int val = int_dist(gen);
     if (val > 50) {
         fmt::print("2 >\n");
-        ib_tail_call(do_print3, ptr);
+        co_do(do_print3, ptr);
     } else {
         fmt::print("2 <=\n");
-        ib_tail_call(do_print2, ptr);
+        co_do(do_print2, ptr);
     }
 }
 
@@ -52,19 +52,19 @@ IB_ASYNC do_print1(void* ptr = nullptr) {
     int val = int_dist(gen);
     if (val > 50) {
         fmt::print("1 >\n");
-        ib_tail_call(do_print2, ptr);
+        co_do(do_print2, ptr);
     } else {
         fmt::print("1 <=\n");
-        ib_tail_call(do_print1, ptr);
+        co_do(do_print1, ptr);
     }
 }
 
 IB_ASYNC async_main(int argc, char** argv) {
     fmt::print("begin\n");
-    ib_tail_call(do_print1, nullptr);
+    co_do(do_print1, nullptr);
 }
 
 int main(int argc, char** argv) {
-    ib_tail_call(async_main, argc, argv);
+    co_do(async_main, argc, argv);
     __builtin_unreachable();
 }
