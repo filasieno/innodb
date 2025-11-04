55421a4bc82855894be58c400e934155a94d66a1
Gitlab CI <fabio.filasieno@gmail.com>
2025-11-05 00:47:29 +0100

    add preserve none

--- xinnodb/src/task/example/tail_call.hpp
+++ xinnodb/src/task/example/tail_call.hpp
@@ -2,8 +2,8 @@
 
 #include <type_traits>
 
-// OPTION 1: With [[noreturn]] - Maximum performance for CPS
-#define IB_ASYNC [[noreturn]] void
+// OPTION 1: With [[preserve_none]] - Maximum performance for CPS
+#define IB_ASYNC [[noreturn]] __attribute__((preserve_none)) void
 
 using reg_t = unsigned long long int;
 
@@ -25,184 +25,99 @@ constexpr bool check_tail_call() {
 
 #define TAIL_CALL_0(fn) do { \
     static_assert(check_tail_call<decltype(fn)>(), "co_do: invalid function signature or argument constraints"); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("leave; jmpq *%0" : : "r"((void*)(__fn)) : "memory"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_1(fn, a1) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)), "r"(r1) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("movq %1, %%r12; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)) : "memory", "r12"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_2(fn, a1, a2) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)), "r"(r1), "r"(r2) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)) : "memory", "rdi", "rsi"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_3(fn, a1, a2, a3) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)), "r"(r1), "r"(r2), "r"(r3) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)) : "memory", "rdi", "rsi", "rdx"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_4(fn, a1, a2, a3, a4) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)), "r"(r1), "r"(r2), "r"(r3), "r"(r4) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)) : "memory", "rdi", "rsi", "rdx", "rcx"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_5(fn, a1, a2, a3, a4, a5) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    asm volatile("leave; jmpq *%0" : : "r"((void*)(fn)), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5) : "memory"); \
+    void* __fn = (void*)(fn); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_6(fn, a1, a2, a3, a4, a5, a6) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_7(fn, a1, a2, a3, a4, a5, a6, a7) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_8(fn, a1, a2, a3, a4, a5, a6, a7, a8) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_9(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8), decltype(a9)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
-    register reg_t r9 asm("r12") = (reg_t)(a9); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8), "r"(r9) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; movq %9, %%r12; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)), "r"((reg_t)(a9)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_10(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8), decltype(a9), decltype(a10)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
-    register reg_t r9 asm("r12") = (reg_t)(a9); \
-    register reg_t r10 asm("r13") = (reg_t)(a10); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8), "r"(r9), "r"(r10) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; movq %9, %%r12; movq %10, %%r13; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)), "r"((reg_t)(a9)), "r"((reg_t)(a10)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12", "r13"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_11(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8), decltype(a9), decltype(a10), decltype(a11)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
-    register reg_t r9 asm("r12") = (reg_t)(a9); \
-    register reg_t r10 asm("r13") = (reg_t)(a10); \
-    register reg_t r11 asm("r14") = (reg_t)(a11); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8), "r"(r9), "r"(r10), "r"(r11) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; movq %9, %%r12; movq %10, %%r13; movq %11, %%r14; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)), "r"((reg_t)(a9)), "r"((reg_t)(a10)), "r"((reg_t)(a11)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12", "r13", "r14"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_12(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8), decltype(a9), decltype(a10), decltype(a11), decltype(a12)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
-    register reg_t r9 asm("r12") = (reg_t)(a9); \
-    register reg_t r10 asm("r13") = (reg_t)(a10); \
-    register reg_t r11 asm("r14") = (reg_t)(a11); \
-    register reg_t r12 asm("r15") = (reg_t)(a12); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8), "r"(r9), "r"(r10), "r"(r11), "r"(r12) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; movq %9, %%r12; movq %10, %%r13; movq %11, %%r14; movq %12, %%r15; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)), "r"((reg_t)(a9)), "r"((reg_t)(a10)), "r"((reg_t)(a11)), "r"((reg_t)(a12)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"); \
     __builtin_unreachable(); \
 } while(0)
 
 #define TAIL_CALL_13(fn, a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13) do { \
     static_assert(check_tail_call<decltype(fn), decltype(a1), decltype(a2), decltype(a3), decltype(a4), decltype(a5), decltype(a6), decltype(a7), decltype(a8), decltype(a9), decltype(a10), decltype(a11), decltype(a12), decltype(a13)>(), "co_do: invalid function signature or argument constraints"); \
-    register reg_t r1 asm("rdi") = (reg_t)(a1); \
-    register reg_t r2 asm("rsi") = (reg_t)(a2); \
-    register reg_t r3 asm("rdx") = (reg_t)(a3); \
-    register reg_t r4 asm("rcx") = (reg_t)(a4); \
-    register reg_t r5 asm("r8")  = (reg_t)(a5); \
-    register reg_t r6 asm("r9")  = (reg_t)(a6); \
-    register reg_t r7 asm("r10") = (reg_t)(a7); \
-    register reg_t r8 asm("r11") = (reg_t)(a8); \
-    register reg_t r9 asm("r12") = (reg_t)(a9); \
-    register reg_t r10 asm("r13") = (reg_t)(a10); \
-    register reg_t r11 asm("r14") = (reg_t)(a11); \
-    register reg_t r12 asm("r15") = (reg_t)(a12); \
-    register reg_t r13 asm("rbx") = (reg_t)(a13); \
     void* __fn = (void*)(fn); \
-    asm volatile("leave; jmpq *%0" : : "r"(__fn), "r"(r1), "r"(r2), "r"(r3), "r"(r4), "r"(r5), "r"(r6), "r"(r7), "r"(r8), "r"(r9), "r"(r10), "r"(r11), "r"(r12), "r"(r13) : "memory"); \
+    asm volatile("movq %1, %%rdi; movq %2, %%rsi; movq %3, %%rdx; movq %4, %%rcx; movq %5, %%r8; movq %6, %%r9; movq %7, %%r10; movq %8, %%r11; movq %9, %%r12; movq %10, %%r13; movq %11, %%r14; movq %12, %%r15; movq %13, %%rbx; leave; jmpq *%0" : : "r"(__fn), "r"((reg_t)(a1)), "r"((reg_t)(a2)), "r"((reg_t)(a3)), "r"((reg_t)(a4)), "r"((reg_t)(a5)), "r"((reg_t)(a6)), "r"((reg_t)(a7)), "r"((reg_t)(a8)), "r"((reg_t)(a9)), "r"((reg_t)(a10)), "r"((reg_t)(a11)), "r"((reg_t)(a12)), "r"((reg_t)(a13)) : "memory", "rdi", "rsi", "rdx", "rcx", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15", "rbx"); \
     __builtin_unreachable(); \
 } while(0)
 
