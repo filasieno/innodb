64f7850838314ca594e394188439101dc7e2e755
Gitlab CI <fabio.filasieno@gmail.com>
2025-11-16 20:07:52 +0100

    Change target name to ib_node_plugin

--- tools/xib-vscode-plugin-native/src/ib_node_plugin.cpp
+++ tools/xib-vscode-plugin-native/src/ib_node_plugin.cpp
@@ -1,15 +1,15 @@
 #include <node_api.h>
 
-#define IB_NODE_PLUGIN_NAME addon
+#define IB_NODE_PLUGIN_NAME ib_node_plugin
 
 // Simple N-API function: returns a greeting string
-napi_value greet(napi_env env, napi_callback_info info) {
+extern "C" napi_value greet(napi_env env, napi_callback_info info) {
   napi_value result;
   napi_create_string_utf8(env, "Hello from native N-API addon!", NAPI_AUTO_LENGTH, &result);
   return result;
 }
 
-napi_value Init(napi_env env, napi_value exports) {
+extern "C" napi_value Init(napi_env env, napi_value exports) {
   napi_value fn;
   napi_create_function(env, NULL, 0, greet, NULL, &fn);
   napi_set_named_property(env, exports, "greet", fn);
